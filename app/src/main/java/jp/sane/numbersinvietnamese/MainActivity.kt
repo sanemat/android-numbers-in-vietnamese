c44c82d03c98910923f996cded2cf4aeb353e5ab
Sanemat <o.gata.ken@gmail.com>
2018-11-11 16:48:19 +0700

    apply value

--- app/src/main/java/jp/sane/numbersinvietnamese/MainActivity.kt
+++ app/src/main/java/jp/sane/numbersinvietnamese/MainActivity.kt
@@ -2,11 +2,21 @@ package jp.sane.numbersinvietnamese
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val results = mapOf("5" to "năm", "6" to "sáu", "103" to "một trăm lẻ ba")
+        val englishWord = 103
+        val key = englishWord.toString()
+        val vietnameseWord = results[key]
+        val keyView = findViewById<TextView>(R.id.key)
+        keyView.text = key
+        val wordView = findViewById<TextView>(R.id.word)
+        wordView.text = vietnameseWord
     }
 }
