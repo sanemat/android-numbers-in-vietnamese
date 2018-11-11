251aa3270bd9147eea74a2b7900d9a5b3ee343b3
Matt (Sanemat) <o.gata.ken@gmail.com>
2018-11-12 00:41:01 +0700

    feat(next): new random result

--- app/src/main/java/jp/sane/numbersinvietnamese/MainActivity.kt
+++ app/src/main/java/jp/sane/numbersinvietnamese/MainActivity.kt
@@ -2,6 +2,7 @@ package jp.sane.numbersinvietnamese
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import java.util.*
 
@@ -13,6 +14,18 @@ class MainActivity : AppCompatActivity() {
 
         val results = mapOf("5" to "năm", "6" to "sáu", "103" to "một trăm lẻ ba")
         val random = Random()
+        val button = findViewById<Button>(R.id.next)
+
+        button.setOnClickListener {
+            val index = random.nextInt(results.size)
+            val key = results.keys.elementAt(index)
+            val vietnameseWord = results[key]
+            val keyView = findViewById<TextView>(R.id.key)
+            keyView.text = key
+            val wordView = findViewById<TextView>(R.id.word)
+            wordView.text = vietnameseWord
+        }
+
         val index = random.nextInt(results.size)
         val key = results.keys.elementAt(index)
         val vietnameseWord = results[key]
