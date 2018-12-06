199a40fed7b68d9225fbb5288c20d589915bd1b2
Matt (Sanemat) <o.gata.ken@gmail.com>
2018-12-06 23:39:47 +0700

    feat(number): specific numbers

--- app/src/main/java/jp/sane/numbersinvietnamese/NumberToVietnamese.kt
+++ app/src/main/java/jp/sane/numbersinvietnamese/NumberToVietnamese.kt
@@ -5,11 +5,22 @@ val normalNumbers = arrayOf(
     "linh", "một", "hai", "ba", "bốn",
     "năm", "sáu", "bảy", "tám", "chín"
 )
+val ten = "mười"
+val specialTen = "mươi"
+val specialFive = "lăm"
+val specialOne = "mốt"
+val hundred = "trăm"
+val thousand = "nghìn"
+val million = "triệu"
 
 @Throws(NotImplementedError::class)
 fun numberToVietnamese(num: Int) : String {
     return when (num) {
         0 -> zero
+        10 -> "${normalNumbers[1]} $ten"
+        100 -> "${normalNumbers[1]} $hundred"
+        1000 -> "${normalNumbers[1]} $thousand"
+        1000000 -> "${normalNumbers[1]} $million"
         in 1..9 -> normalNumbers[num]
         103 -> "một trăm lẻ ba"
         else -> throw NotImplementedError("unknown: $num")
--- app/src/test/java/jp/sane/numbersinvietnamese/NumberToVietnameseTest.kt
+++ app/src/test/java/jp/sane/numbersinvietnamese/NumberToVietnameseTest.kt
@@ -10,6 +10,10 @@ import org.junit.Assert.*
  */
 class NumberToVietnameseTest {
     @Test
+    fun num0() {
+        assertEquals("không", numberToVietnamese(0))
+    }
+    @Test
     fun num5() {
         assertEquals("năm", numberToVietnamese(5))
     }
@@ -18,7 +22,23 @@ class NumberToVietnameseTest {
         assertEquals("sáu", numberToVietnamese(6))
     }
     @Test
+    fun num10() {
+        assertEquals("một mười", numberToVietnamese(10))
+    }
+    @Test
+    fun num100() {
+        assertEquals("một trăm", numberToVietnamese(100))
+    }
+    @Test
     fun num103() {
         assertEquals("một trăm lẻ ba", numberToVietnamese(103))
+    }
+    @Test
+    fun num1000() {
+        assertEquals("một nghìn", numberToVietnamese(1000))
+    }
+    @Test
+    fun num1000000() {
+        assertEquals("một triệu", numberToVietnamese(1000000))
     }
 }
