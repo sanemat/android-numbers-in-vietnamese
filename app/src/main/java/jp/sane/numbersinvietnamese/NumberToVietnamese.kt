94fcdf65c8723159e2242e60d7bd6d88baf57b93
Matt (Sanemat) <o.gata.ken@gmail.com>
2018-12-06 23:24:31 +0700

    feat(vietnamese): 0-9

--- app/src/main/java/jp/sane/numbersinvietnamese/NumberToVietnamese.kt
+++ app/src/main/java/jp/sane/numbersinvietnamese/NumberToVietnamese.kt
@@ -1,10 +1,16 @@
 package jp.sane.numbersinvietnamese
 
+val zero = "không"
+val normalNumbers = arrayOf(
+    "linh", "một", "hai", "ba", "bốn",
+    "năm", "sáu", "bảy", "tám", "chín"
+)
+
 @Throws(NotImplementedError::class)
 fun numberToVietnamese(num: Int) : String {
     return when (num) {
-        5 -> "năm"
-        6 -> "sáu"
+        0 -> zero
+        in 1..9 -> normalNumbers[num]
         103 -> "một trăm lẻ ba"
         else -> throw NotImplementedError("unknown: $num")
     }
